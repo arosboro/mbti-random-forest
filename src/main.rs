e97e0d105b0d9ad463285cc2422fe5399ee4f700
Andrew Rosborough <arosborough@oculat.us>
2022-09-04 14:07:35 -0400

    Break down main code into independant functions for clarity.

--- src/main.rs
+++ src/main.rs
@@ -103,13 +103,13 @@ fn tokenize(post: &str) -> Post {
   tokens
 }
 
-fn main() -> Result<(), Error> {
+fn load_data() -> Result<Vec<Sample>, Error> {
   let mut reader = csv::Reader::from_path("./mbti_1.csv").unwrap();
-  let mut training_set: Vec<Sample> = Vec::new();
+  let mut samples: Vec<Sample> = Vec::new();
   for row in reader.deserialize::<Row>() {
     match row {
       Ok(row) => {
-        let mut sample = Sample {
+        let mut sample: Sample = Sample {
           indicator: MBTI::from_string(&row.label),
           posts: Vec::new(),
         };
@@ -117,21 +117,35 @@ fn main() -> Result<(), Error> {
           sample.posts.push(tokenize(post));
         }
         // println!("{}: {} posts", sample.indicator.to_string(), sample.posts.len());
-        training_set.push(sample)
+        samples.push(sample)
       },
       Err(e) => println!("Error: {}", e),
     }
   }
+  Ok(samples)
+}
 
-  // Flatten samples as x_set and y_set.
+fn normalize(training_set: &Vec<Sample>) -> (Vec<Vec<f64>>, Vec<u8>) {
   let mut x_set: Vec<Post> = Vec::new();
   let mut y_set: Vec<MBTI> = Vec::new();
+
   for sample in training_set {
-    for post in sample.posts {
-      x_set.push(post);
+    for post in &sample.posts {
+      x_set.push(post.to_vec());
       y_set.push(sample.indicator);
     }
   }
+  println!("{} samples", x_set.len());
+
+  // Deterimine unique labels
+  let mut unique_labels: Vec<String> = Vec::new();
+  for label in y_set.iter() {
+    if !unique_labels.contains(&label.to_string()) {
+      unique_labels.push(label.to_string());
+    }
+  }
+  println!("{} unique labels", unique_labels.len());
+
 
   // Create a dictionary indexing unique tokens.
   let mut dictionary: Vec<String> = Vec::new();
@@ -142,6 +156,14 @@ fn main() -> Result<(), Error> {
       }
     }
   }
+  println!("Dictionary size: {}", dictionary.len());
+  println!("Saving dictionary...");
+  // Save dictionary
+  let dictionary_bytes = bincode::serialize(&dictionary).expect("Can not serialize the model");
+        File::create("dictionary.bincode")
+          .and_then(|mut f| f.write_all(&dictionary_bytes))
+          .expect("Can not persist dictionary");
+
 
   // Create f64 matrices for x_set.
   let mut x_matrix: Vec<Vec<f64>> = Vec::new();
@@ -155,29 +177,66 @@ fn main() -> Result<(), Error> {
   }
 
   // Create f64 matrices for y_set.
-  let mut y_matrix: Vec<f64> = Vec::new();
+  let mut y_matrix: Vec<u8> = Vec::new();
   for indicator in y_set {
-    let index: f64 = indicator.indicator as f64;
+    let index: u8 = indicator.indicator;
     y_matrix.push(index);
   }
 
+  println!("Saving matrices...");
+  let x_matrix_bytes = bincode::serialize(&x_matrix).expect("Can not serialize the matrix");
+        File::create("x_matrix.bincode")
+          .and_then(|mut f| f.write_all(&x_matrix_bytes))
+          .expect("Can not persist x_matrix");
+  let y_matrix_bytes: Vec<u8> = bincode::serialize(&y_matrix).expect("Can not serialize the matrix");
+        File::create("y_matrix.bincode")
+          .and_then(|mut f| f.write_all(&y_matrix_bytes))
+          .expect("Can not persist y_matrix");
+
+  (x_matrix, y_matrix)
+}
+
+fn tally(y_matrix: &Vec<u8>) {
+  println!("count ISTJ: {}", y_matrix.iter().filter(|&m| *m == indicator::ISTJ).count());
+  println!("count ISFJ: {}", y_matrix.iter().filter(|&m| *m == indicator::ISFJ).count());
+  println!("count INFJ: {}", y_matrix.iter().filter(|&m| *m == indicator::INFJ).count());
+  println!("count INTJ: {}", y_matrix.iter().filter(|&m| *m == indicator::INTJ).count());
+  println!("count ISTP: {}", y_matrix.iter().filter(|&m| *m == indicator::ISTP).count());
+  println!("count ISFP: {}", y_matrix.iter().filter(|&m| *m == indicator::ISFP).count());
+  println!("count INFP: {}", y_matrix.iter().filter(|&m| *m == indicator::INFP).count());
+  println!("count INTP: {}", y_matrix.iter().filter(|&m| *m == indicator::INTP).count());
+  println!("count ESTP: {}", y_matrix.iter().filter(|&m| *m == indicator::ESTP).count());
+  println!("count ESFP: {}", y_matrix.iter().filter(|&m| *m == indicator::ESFP).count());
+  println!("count ENFP: {}", y_matrix.iter().filter(|&m| *m == indicator::ENFP).count());
+  println!("count ENTP: {}", y_matrix.iter().filter(|&m| *m == indicator::ENTP).count());
+  println!("count ESTJ: {}", y_matrix.iter().filter(|&m| *m == indicator::ESTJ).count());
+  println!("count ESFJ: {}", y_matrix.iter().filter(|&m| *m == indicator::ESFJ).count());
+  println!("count ENFJ: {}", y_matrix.iter().filter(|&m| *m == indicator::ENFJ).count());
+  println!("count ENTJ: {}", y_matrix.iter().filter(|&m| *m == indicator::ENTJ).count());
+}
+
+
+fn train(x_matrix: &Vec<Vec<f64>>, y_matrix: &Vec<u8>) {
   let x: DenseMatrix<f64> = DenseMatrix::from_2d_vec(&x_matrix);
   // These are our target class labels
-  let y: Vec<f64> = y_matrix;
-  // Split dataset into training/test (80%/20%)
+  let y: Vec<f64> = y_matrix.into_iter().map(|x| *x as f64).collect();
+  // Split bag into training/test (80%/20%) TODO: Does it mater we do this every training cycle?
   let (x_train, x_test, y_train, y_test) = train_test_split(&x, &y, 0.2, true);
+
   // Random Forest
-  let y_hat_rf = RandomForestRegressor::fit(&x_train, &y_train, Default::default())
-      .and_then(|rf| {
-        let rf_bytes = bincode::serialize(&rf).expect("Can not serialize the model");
-        File::create("mbti_rf.model")
-          .and_then(|mut f| f.write_all(&rf_bytes))
-          .expect("Can not persist model");
-        rf.predict(&x_test)
-      }).unwrap();
-  // Calculate test error
-  println!("MSE: {}", mean_squared_error(&y_test, &y_hat_rf));
-  // Load the model
+  RandomForestRegressor::fit(&x_train, &y_train, Default::default())
+    .into_iter()
+    .for_each(|rf| {
+      let y_pred = rf.predict(&x_test).unwrap();
+      let accuracy = accuracy(&y_test, &y_pred);
+      println!("Accuracy: {}", accuracy);
+      let rf_bytes = bincode::serialize(&rf).expect("Can not serialize the model");
+      let f = std::fs::OpenOptions::new().write(true).create(true).truncate(true).open("mbti_rf.model");
+      f.and_then(|mut f| f.write_all(&rf_bytes))
+        .expect("Can not persist model");
+    });
+  
+  // Load the Model
   let rf: RandomForestRegressor<f64> = {
     let mut buf: Vec<u8> = Vec::new();
     File::open("mbti_rf.model")
@@ -185,10 +244,17 @@ fn main() -> Result<(), Error> {
       .expect("Can not load model");
     bincode::deserialize(&buf).expect("Can not deserialize the model")
   };
-  // Predict Class Labels
-  let y_hat = rf.predict(&x).unwrap();
-  // Calculate Training Error
-  println!("accuracy: {}", accuracy(&y_test, &y_hat));
+    
+  let y_hat_rf = rf.predict(&x_test).unwrap();
+  // Calculate test error
+  println!("MSE: {}", mean_squared_error(&y_test, &y_hat_rf));
+}
+
+fn main() -> Result<(), Error> {
+  let training_set: Vec<Sample> = load_data()?;
+  let (x_matrix, y_matrix) = normalize(&training_set);
+  tally(&y_matrix);
+  train(&x_matrix, &y_matrix);
 
   Ok(()) 
 }
